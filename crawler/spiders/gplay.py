dcc8b9a238ca8dda2d5fc9020b2e0b0bebea4379
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-04-14 07:49:42 +0530

    displaying last updated and installs

--- crawler/spiders/gplay.py
+++ crawler/spiders/gplay.py
@@ -12,8 +12,15 @@ class GplaySpider(scrapy.Spider):
     def parse(self, response):
         page = response.url.split("/")[-1]
         name = response.css('h1.AHFaub')
+        downloads = response.css('span.htlgb')
+        downloads = downloads.css('span::text').getall()
+        # for x in downloads:
+        #     print("=======>",x)
         yield{
-            'app name': name.css('span::text').get(),
+            'App Name': name.css('span::text').get(),
+            'Updated': downloads[0],
+            'Installs': downloads[4],
+            # 'Reviews': reviews.css('span::text').get(),
         }
         next_app = response.css('div.wXUyZd a::attr(href)').get()
         if next_app is not None:
