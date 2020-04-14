1bacc41bb314fdb7d9f826a9e7b3ed8a20546388
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-04-14 07:29:48 +0530

    storing names of a given app

--- crawler/spiders/gplay.py
+++ crawler/spiders/gplay.py
@@ -15,9 +15,9 @@ class GplaySpider(scrapy.Spider):
         yield{
             'app name': name.css('span::text').get(),
         }
-        # next_app = response.css('div.wXUyZd a::attr(href)').get()
-        # if next_app is not None:
-        #     next_app = response.urljoin(next_app)
-        #     yield scrapy.Request(next_app, callback=self.parse)
+        next_app = response.css('div.wXUyZd a::attr(href)').get()
+        if next_app is not None:
+            next_app = response.urljoin(next_app)
+            yield scrapy.Request(next_app, callback=self.parse)
 
             
